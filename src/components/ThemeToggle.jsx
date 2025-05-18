bf6a9ce12c2640645d058e55384f162ddf1d78f9
Rangel Shishkov <rshishkov01@gmail.com>
2025-05-18 23:51:30 +0300

    fix theme toggle btn

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -35,7 +35,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-2 right-5 z-50 p-2 rounded-full",
+        "fixed max-sm:hidden top-1.5 right-2 2xl:right-6 z-50 p-2 rounded-full",
         "transition-colors duration-300 focus:outline-hidden"
       )}
     >
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,11 @@ import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectSection";
 import { SkillsSection } from "../components/SkillsSection";
-import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 
 export const Home = () => {
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      {/* Toggle Theme*/}
-      <ThemeToggle />
       {/* BG Effects */}
       <StarBackground />
       {/* Navbar */}
